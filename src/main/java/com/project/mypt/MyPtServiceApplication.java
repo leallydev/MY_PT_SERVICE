b15866ee83ed0d9edd3c45b975a8ce775f18b168
leallydev <rlatpfls3707@naver.com>
2023-02-01 15:35:08 +0900

    project first setting

---
+++ src/main/java/com/project/mypt/MyPtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.mypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPtServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mypt/MyPtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
